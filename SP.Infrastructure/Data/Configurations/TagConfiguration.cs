b931036e70ec9ba9371879b61265013fd4e9e836
jeangreg <jean.g.apprenant@gmail.com>
2026-06-02 12:50:10 +0200

    Challenge

---
+++ SP.Infrastructure/Data/Configurations/TagConfiguration.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SP.Domain.Entities;
+
+namespace SP.Infrastructure.Data.Configurations;
+
+public class TagConfiguration : IEntityTypeConfiguration<Tag>
+{
+    public void Configure(EntityTypeBuilder<Tag> builder)
+    {
+        builder.ToTable("Tags");
+
+        builder.HasKey(t => t.TagsId);
+        
+        builder.Property(t => t.TagsId)
+            .HasColumnName("TagsId")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(t => t.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(t => t.created_at)
+            .HasColumnName("created_at")
+            .IsRequired()
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+    }
+}
